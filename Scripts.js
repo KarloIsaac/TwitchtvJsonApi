f1cc6f31c5fab88b3eb90ae88177f671a971f516
KarloIsaac <qafqunam2001@gmail.com>
2017-11-28 09:02:17 -0600

    Created the .js and .html files and a .txt file with information to work on the project. Added a function to start testing the Twitch API Response.

---
+++ Scripts.js
@@ -0,0 +1,8 @@
+function retrieveChannelsResponse() {
+    var targetUsersArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb",
+            "noobs2ninjas"];
+    targetUsersArray.forEach(function(user, index, array) {
+        console.log(user);
+    });
+
+}
