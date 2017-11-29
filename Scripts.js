d3b624bffd64d2637d77039c2fbaa820c4e1c489
KarloIsaac <qafqunam2001@gmail.com>
2017-11-29 08:43:28 -0600

    Just refactored the code and created a retrieveJsonResponseForUser() function

--- Scripts.js
+++ Scripts.js
@@ -2,7 +2,23 @@ function retrieveChannelsResponse() {
     var targetUsersArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb",
             "noobs2ninjas"];
     targetUsersArray.forEach(function(user, index, array) {
-        console.log(user);
-    });
 
+    });
+    retrieveJsonResponseForUser("RobotCaleb");
+}
+
+
+function retrieveJsonResponseForUser(user) {
+    var requestUrl =  "https://wind-bow.gomix.me/twitch-api/streams/" + user + "";
+    console.log(requestUrl);
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = function() {
+        if(xmlHttpRequest.readyState === 4 && xmlHttpRequest.status === 200) {
+            jsonResponse = JSON.parse(xmlHttpRequest.responseText);
+            console.log(jsonResponse);
+        }
+    };
+    xmlHttpRequest.open("GET", requestUrl, true);
+    //xmlHttpRequest.setRequestHeader("Access-Control-Allow-Origin", "inventado-no-nulo");
+    xmlHttpRequest.send();
 }
