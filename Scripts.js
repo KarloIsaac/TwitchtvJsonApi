923ef6b491be1458ca891f99a7fb89fba6aaf449
KarloIsaac <qafqunam2001@gmail.com>
2017-11-30 08:13:25 -0600

    Created a function to request user information through JSONP. Started laying out the function that will translate the user info into html elements. Added an icon to represent when the user is offline

--- Scripts.js
+++ Scripts.js
@@ -1,24 +1,33 @@
 function retrieveChannelsResponse() {
-    var targetUsersArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb",
+    var targetUsersArray = ["freecodecamp", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb",
             "noobs2ninjas"];
     targetUsersArray.forEach(function(user, index, array) {
-
+        requestUserInformation(user);
     });
-    retrieveJsonResponseForUser("RobotCaleb");
+
 }
 
 
-function retrieveJsonResponseForUser(user) {
-    var requestUrl =  "https://wind-bow.gomix.me/twitch-api/streams/" + user + "";
-    console.log(requestUrl);
-    var xmlHttpRequest = new XMLHttpRequest();
-    xmlHttpRequest.onreadystatechange = function() {
-        if(xmlHttpRequest.readyState === 4 && xmlHttpRequest.status === 200) {
-            jsonResponse = JSON.parse(xmlHttpRequest.responseText);
-            console.log(jsonResponse);
-        }
-    };
-    xmlHttpRequest.open("GET", requestUrl, true);
-    //xmlHttpRequest.setRequestHeader("Access-Control-Allow-Origin", "inventado-no-nulo");
-    xmlHttpRequest.send();
+function requestUserInformation(userName) {
+    body = document.getElementsByTagName("body")[0];
+    scriptElement = document.createElement("script");
+    scriptElement.src = "https://wind-bow.glitch.me/twitch-api/streams/" + userName +"?callback=processUserInformation";
+    body.append(scriptElement);
+    body.removeChild(scriptElement);
+}
+
+
+function processUserInformation(userJsonData) {
+
+    userPanel = document.createElement("div");
+    userPanel.class = "user-icon";
+
+    userIcon = document.createElement("img");
+    userIcon.src = "https://cdn3.iconfinder.com/data/icons/cat-force/256/cat_sleep.png";
+    userIcon.height = 150;
+    userIcon.width = 150;
+
+    body = document.getElementsByTagName("body")[0];
+    body.append(userIcon);
+    console.log(userJsonData);
 }
