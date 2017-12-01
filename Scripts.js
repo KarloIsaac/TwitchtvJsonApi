626635e5bd107cff39d7e08215344b3ed06e6c41
KarloIsaac <qafqunam2001@gmail.com>
2017-12-01 09:07:42 -0600

    If the user is online, their channel icon is displayed, otherwise a default icon will. For every user an information panel is created, which contins the name of the user, wheter they are online or not.

--- Scripts.js
+++ Scripts.js
@@ -1,33 +1,64 @@
 function retrieveChannelsResponse() {
-    var targetUsersArray = ["freecodecamp", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb",
+    var channelIdArray = ["freecodecamp", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb",
             "noobs2ninjas"];
-    targetUsersArray.forEach(function(user, index, array) {
-        requestUserInformation(user);
+    channelIdArray.forEach(function(channeId, index, array) {
+        requestChannelInformation(channeId);
     });
 
 }
 
 
-function requestUserInformation(userName) {
-    body = document.getElementsByTagName("body")[0];
-    scriptElement = document.createElement("script");
-    scriptElement.src = "https://wind-bow.glitch.me/twitch-api/streams/" + userName +"?callback=processUserInformation";
+function requestChannelInformation(channeId) {
+    var body = document.getElementsByTagName("body")[0];
+    var scriptElement = document.createElement("script");
+    scriptElement.src = "https://wind-bow.glitch.me/twitch-api/streams/" + channeId +
+            "?callback=processChannelInformation";
     body.append(scriptElement);
     body.removeChild(scriptElement);
 }
 
 
-function processUserInformation(userJsonData) {
+function processChannelInformation(userJsonData) {
+console.log(userJsonData);
+    var stream = userJsonData.stream;
+    var channelId = retrieveChannelId(userJsonData._links.channel);
+    var channel = stream === null ? null : stream.channel;
+    var channelIcon = createChannelIconElement(channel);
 
-    userPanel = document.createElement("div");
-    userPanel.class = "user-icon";
+    var channelPanel = document.createElement("div");
+    channelPanel.className  = "channel-panel";
+    channelPanel.append(channelIcon);
+    channelPanel.append(createInformationPanelElement(channel, channelId));
 
-    userIcon = document.createElement("img");
-    userIcon.src = "https://cdn3.iconfinder.com/data/icons/cat-force/256/cat_sleep.png";
+    var channelsPanel = document.getElementById("channels-holder");
+    channelsPanel.append(channelPanel);
+}
+
+
+function retrieveChannelId(channelLink) {
+    var pattern = /[^\/]*$/;
+    var match = pattern.exec(channelLink);
+    var channelId = match[0];
+    return channelId;
+}
+
+
+function createChannelIconElement(channel) {
+    var imageAddress = channel === null
+            ? "https://cdn3.iconfinder.com/data/icons/cat-force/256/cat_sleep.png"
+            : channel.logo;
+    var userIcon = document.createElement("img");
+    userIcon.src = imageAddress;
     userIcon.height = 150;
     userIcon.width = 150;
+    return userIcon;
+}
 
-    body = document.getElementsByTagName("body")[0];
-    body.append(userIcon);
-    console.log(userJsonData);
+
+function createInformationPanelElement(channel, channelId) {
+    var channelName = document.createElement("h2");
+    channelName.innerText = channelId;
+    var informationPanel = document.createElement("div");
+    informationPanel.append(channelName);
+    return informationPanel;
 }
